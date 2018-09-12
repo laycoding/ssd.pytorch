9b05ccddc00bb31d937817d5b468487d94d381c0
Weidong Zhang <battle975444818@outlook.com>
2018-09-12 16:05:59 +0800

    x

--- layers/modules/__init__.py
+++ layers/modules/__init__.py
@@ -1,4 +1,5 @@
 from .l2norm import L2Norm
 from .multibox_loss import MultiBoxLoss
-
+from .recall_loss import RecallLoss
+from .precision_loss import PrecisionLoss
 __all__ = ['L2Norm', 'MultiBoxLoss', 'RecallLoss', 'PrecisionLoss']
